a439e38fa874ae79ff7f293fa3bb8c5bc5fd531e
Chockalingam Kasi <chocka1998@gmail.com>
2022-05-13 23:13:07 +0800

    draft navbar

--- energize_app/src/App.js
+++ energize_app/src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import MapComp from './components/mapComponent'
-
+import React from "react"
+import Signup from "./components/Signup"
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/ForgotPassword"
 
 function App() {
   return (
-    <div className="App">
-      <MapComp></MapComp>
-    </div>
-  );
+
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+
+  )
 }
 
-export default App;
+export default App

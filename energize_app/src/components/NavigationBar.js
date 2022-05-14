9ba30926d815fca02c1e293c7fc936e2edcbec24
Chockalingam Kasi <chocka1998@gmail.com>
2022-05-14 11:00:15 +0800

    Update NavigationBar.js

--- energize_app/src/components/NavigationBar.js
+++ energize_app/src/components/NavigationBar.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
 
 const NavigationBar = () => {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   async function handleLogout() {
     setError("");
@@ -24,14 +29,7 @@ const NavigationBar = () => {
     <Navbar bg="light" expand="lg" style={{ fontSize: "1.2rem" }}>
       <Container>
         <Navbar.Brand href="/">
-          <img
-            src="./logo.jpg"
-            width="30"
-            height="30"
-            class="d-inline-block align-top"
-            alt=""
-          />
-          &nbsp;&nbsp;Energize
+          Energize
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -41,15 +39,33 @@ const NavigationBar = () => {
             <Nav.Link href="/Transactions">Transactions</Nav.Link>
             <Nav.Link href="/SolarPanels">Buy/Rent Solar Panels</Nav.Link>
           </Nav>
-          <Nav>
-            <NavDropdown title={currentUser.email} id="basic-nav-dropdown">
-              <Nav.Link href="/Profile">Profile</Nav.Link>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <Nav.Link href="#" onClick={handleLogout}>
-                Log Out
-              </Nav.Link>
-            </NavDropdown>
-          </Nav>
+          <>
+            <Button variant="outline-light" onClick={handleShow}>
+            <img
+            src="./logo.jpg"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title >User Info</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{currentUser.email}</Modal.Body>
+              <Modal.Footer>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleLogout}>
+                  Log Out
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
         </Navbar.Collapse>
       </Container>
     </Navbar>

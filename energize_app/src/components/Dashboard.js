fce9415b2a5e353ec1c0ed7a08fb52efeb4d80fc
lishengjaw <79784988+lishengjaw@users.noreply.github.com>
2022-05-14 11:01:07 +0800

    Refactor

--- energize_app/src/components/Dashboard.js
+++ energize_app/src/components/Dashboard.js
@@ -1,15 +1,13 @@
-import React from "react"
-import NavigationBar from "./NavigationBar"
+import React from "react";
+import NavigationBar from "./NavigationBar";
 
 export default function Dashboard() {
- 
-
   return (
     <>
-    <NavigationBar/> 
-    <div class="col d-flex justify-content-center">
+      <NavigationBar />
+      <div className="col d-flex justify-content-center">
         <h1>Temporary Dashboard Page</h1>
-    </div>
+      </div>
     </>
-  )
+  );
 }
--- energize_app/src/components/Profile.js
+++ energize_app/src/components/Profile.js
@@ -1,29 +1,16 @@
 import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
+import { Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
 import NavigationBar from "./NavigationBar"
 
 export default function Profile() {
   const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const navigate = useNavigate()
-
-  async function handleLogout() {
-    setError("")
-
-    try {
-      await logout()
-      navigate("/login")
-    } catch {
-      setError("Failed to log out")
-    }
-  }
+  const { currentUser } = useAuth()
 
   return (
     <>
     <NavigationBar/>
-    <div class="col d-flex justify-content-center">
+    <div className="col d-flex justify-content-center">
             <h1>Temporary Profile Page</h1>
         </div>
       <Card>
@@ -31,16 +18,8 @@ export default function Profile() {
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
     </>
   )
 }

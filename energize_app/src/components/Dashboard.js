4935237db400415c7ca40802fb47b29703dadb64
Chockalingam Kasi <chocka1998@gmail.com>
2022-05-14 09:08:31 +0800

    Added Top Right Profile Component

--- energize_app/src/components/Dashboard.js
+++ energize_app/src/components/Dashboard.js
@@ -1,46 +1,15 @@
-import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
+import React from "react"
 import NavigationBar from "./NavigationBar"
 
 export default function Dashboard() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const navigate = useNavigate()
-
-  async function handleLogout() {
-    setError("")
-
-    try {
-      await logout()
-      navigate("/login")
-    } catch {
-      setError("Failed to log out")
-    }
-  }
+ 
 
   return (
     <>
-    <NavigationBar/>
+    <NavigationBar/> 
     <div class="col d-flex justify-content-center">
-            <h1>Temporary Dashboard Page</h1>
-        </div>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
+        <h1>Temporary Dashboard Page</h1>
+    </div>
     </>
   )
 }

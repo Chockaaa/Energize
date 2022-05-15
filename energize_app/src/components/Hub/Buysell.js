6caae156ff0cd1730204d7081588f6dd937848cd
Chockalingam Kasi <chocka1998@gmail.com>
2022-05-15 13:01:36 +0800

    add buy sell component

--- energize_app/src/components/Hub/Buysell.js
+++ energize_app/src/components/Hub/Buysell.js
@@ -1,11 +1,47 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
+import { Container, Card } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function BuySell() {
   return (
     <>
       <NavigationBar />
-      <div>Hello Buy Sell Energy Portal</div>
+      <Container >
+        <Row className="mx-auto my-5">
+          <div className="col d-flex justify-content-center">
+            <h1>Energy Hub Services</h1>
+          </div>
+        </Row>
+        <Row xs={1} md={2} className="g-4" style={{marginTop:"4rem", marginBottom:"6rem"}}>
+          <Col>
+            <Card border="primary" style={{ width: "32rem", height: "18rem" }}>
+              <Card.Body>
+                <Card.Title style={{textAlign:"center", fontFamily:"monospace",textTransform:"uppercase"}}>Buy Electricity</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card border="primary" style={{ width: "32rem", height: "18rem" }}>
+              <Card.Body>
+                <Card.Title style={{textAlign:"center", fontFamily:"monospace",textTransform:"uppercase"}}>Sell Electricity</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }

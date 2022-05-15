19bab7cd739f237395f243757b26353892a20203
yixiong321 <yong053@e.ntu.edu.sg>
2022-05-16 00:29:26 +0800

    editing ui

--- energize_app/src/components/Client/Booking.js
+++ energize_app/src/components/Client/Booking.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NavigationBar from "./NavigationBar";
-import { Button, Form, Card, Modal } from "react-bootstrap"
+import { Button, Form, Card, Modal, Container, Row, Carousel, Col, Image } from "react-bootstrap"
 import { Outlet, useParams } from "react-router-dom";
 import { getHubFromName } from '../../db/HubsDB'
 import { addTransaction } from "../../db/TransactionsDB";
@@ -14,13 +14,18 @@ const Booking = () => {
   const { currentUser } = useAuth()
   const [selectValue, setSelectValue] = useState(null)
   const [currCap, setCurrCap] = useState(null)
-  const [currToCred,setCurrToCred] = useState(null)
+  const [currToCred, setCurrToCred] = useState(null)
   const [amt, setAmt] = useState('')
   const [convRate, setConvRate] = useState(null)
-  const [docId,setDocId] = useState(null)
-
+  const [docId, setDocId] = useState(null)
+  const imgSources = {
+    "1001": "/images/1001.jpg",
+    "1002": "/images/1002.jpg",
+    "1003": "/images/1003.jpg",
+    "1004": "/images/1004.jpg"
+  }
   useEffect(() => {
-    
+
     if (selectedHubName) {
       getHubFromName(selectedHubName).then((res) => {
         setCurrToCred(res[0]._delegate._document.data.value.mapValue.fields.currencyToCredits.integerValue)
@@ -57,20 +62,20 @@ const Booking = () => {
   };
   const handleShow = (e) => {
     e.preventDefault();
-    if(amt!=''){
+    if (amt != '') {
       setShow(true);
     }
-    
+
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
-      cost: amt * convRate, 
-      creditsEarned: -1*currToCred*amt*convRate,
+      cost: amt * convRate,
+      creditsEarned: -1 * currToCred * amt * convRate,
       dateCompleted: "",
       energyAmount: parseInt(amt),
-      hubId: parseInt(docId), 
+      hubId: parseInt(docId),
       status: "Pending",
       transactionType: 0,
       userName: currentUser.email
@@ -99,98 +104,116 @@ const Booking = () => {
   return (
     <>
       <NavigationBar />
-      {selectedHubName ?
-        //form with preselected hub
-        <Card className='p-3 m-3s'>
-          <Form>
-            <Form.Group className="mb-3" controlId="hubSelect">
-              <Form.Label>Hub Name</Form.Label>
-              <Form.Control
-                as="select"
-                defaultValue={selectedHubName}
-                onChange={(e) => { handleSelect(e.target.value) }}>
-                <option value={'Alpha'}>Alpha</option>
-                <option value={'Bravo'}>Bravo</option>
-                <option value={'Charlie'}>Charlie</option>
-                <option value={'Delta'}>Delta</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="amtToBuy">
-              <Form.Label>Amount to Buy(W) (Available Capacity: {currCap})</Form.Label>
-              <Form.Control
-                required
-                type="number"
-                value={amt}
-                onChange={(e) => { handleChange(e.target.value) }}
-                placeholder="Amount to Buy in Watts" />
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={(e)=>handleShow(e)}>
-              Submit
+      <Container className='mt-5 p-3 center'>
+        {selectedHubName ?
+          //form with preselected hub
+          <Card  >
+            <Container>
+              <Row>
+                <Col className='center'><img width='408px' height='275px'
+                  src={imgSources[docId]}></img></Col>
+                <Col>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="hubSelect">
+                      <Form.Label>Hub Name</Form.Label>
+                      <Form.Control
+                        as="select"
+                        defaultValue={selectedHubName}
+                        onChange={(e) => { handleSelect(e.target.value) }}>
+                        <option value={'Alpha'}>Alpha</option>
+                        <option value={'Bravo'}>Bravo</option>
+                        <option value={'Charlie'}>Charlie</option>
+                        <option value={'Delta'}>Delta</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="amtToBuy">
+                      <Form.Label>Amount to Buy(W) (Available Capacity: {currCap})</Form.Label>
+                      <Form.Control
+                        required
+                        type="number"
+                        value={amt}
+                        onChange={(e) => { handleChange(e.target.value) }}
+                        placeholder="Amount to Buy in Watts" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={(e) => handleShow(e)}>
+                      Submit
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
+          </Card>
+          :
+          //fresh form
+          <Card className='p-3 m-3 center' >
+            <Container>
+              <Row>
+                <Col><Image className='rounded' width='408px' height='275px' src={imgSources[docId]}></Image></Col>
+                <Col>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="hubSelect">
+                      <Form.Label>Hub Name</Form.Label>
+                      <Form.Control
+                        as="select"
+                        defaultValue={'Alpha'}
+                        onChange={(e) => { handleSelect(e.target.value) }}
+                      >
+                        <option value={'Alpha'}>Alpha</option>
+                        <option value={'Bravo'}>Bravo</option>
+                        <option value={'Charlie'}>Charlie</option>
+                        <option value={'Delta'}>Delta</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="amtToBuy">
+                      <Form.Label>Amount to Buy (W) (Available Capacity: {currCap})</Form.Label>
+                      <Form.Control
+                        required
+                        type="number"
+                        value={amt}
+                        onChange={(e) => { handleChange(e.target.value) }}
+                        placeholder="Amount to Buy in Watts" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={(e) => handleShow(e)}>
+                      Submit
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
+
+          </Card>
+
+
+        }
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Booking</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Hub Name: {selectValue}
+            <p></p>
+            Amount: {amt}
+            <p></p>
+            Credits Required: {amt * convRate}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" size="sm" onClick={handleClose}>
+              No
             </Button>
-          </Form>
-
-        </Card>
-        :
-        //fresh form
-        <Card className='p-3 m-3s'>
-          <Form>
-            <Form.Group className="mb-3" controlId="hubSelect">
-              <Form.Label>Hub Name</Form.Label>
-              <Form.Control
-                as="select"
-                defaultValue={'Alpha'}
-                onChange={(e) => { handleSelect(e.target.value) }}
-              >
-                <option value={'Alpha'}>Alpha</option>
-                <option value={'Bravo'}>Bravo</option>
-                <option value={'Charlie'}>Charlie</option>
-                <option value={'Delta'}>Delta</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="amtToBuy">
-              <Form.Label>Amount to Buy(W) (Available Capacity: {currCap})</Form.Label>
-              <Form.Control
-                required
-                type="number"
-                value={amt}
-                onChange={(e) => { handleChange(e.target.value) }}
-                placeholder="Amount to Buy in Watts" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit" onClick={(e)=>handleShow(e)}>
-              Submit
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={(e) => handleSubmit(e)}
+            >
+              yes
             </Button>
-          </Form></Card>
+          </Modal.Footer>
+        </Modal>
+        <Outlet></Outlet>
 
-
-      }
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm Booking</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Hub Name: {selectValue}
-          <p></p>
-          Amount: {amt}
-          <p></p>
-          Credits Required: {amt * convRate}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" size="sm" onClick={handleClose}>
-            No
-          </Button>
-          <Button
-            variant="primary"
-            size="sm"
-            onClick={(e) => handleSubmit(e)}
-          >
-            yes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Outlet></Outlet>
-
+      </Container>
     </>
+
   );
 };
 

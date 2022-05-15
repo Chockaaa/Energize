9b31f6b48e1cbc601b30e50de68e82096d09dfcc
Chockalingam Kasi <chocka1998@gmail.com>
2022-05-15 14:07:55 +0800

    Update Buysell.js

--- energize_app/src/components/Hub/Buysell.js
+++ energize_app/src/components/Hub/Buysell.js
@@ -19,11 +19,7 @@ export default function BuySell() {
             <Card border="primary" style={{ width: "32rem", height: "18rem" }}>
               <Card.Body>
                 <Card.Title style={{textAlign:"center", fontFamily:"monospace",textTransform:"uppercase"}}>Buy Electricity</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
+                
               </Card.Body>
             </Card>
           </Col>
@@ -32,11 +28,6 @@ export default function BuySell() {
             <Card border="primary" style={{ width: "32rem", height: "18rem" }}>
               <Card.Body>
                 <Card.Title style={{textAlign:"center", fontFamily:"monospace",textTransform:"uppercase"}}>Sell Electricity</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
